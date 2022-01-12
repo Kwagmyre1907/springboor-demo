aae8367c4e8f2ea90438cae0c2c73a8af248a092
Rayno Hattingh <raynohattingh101@gmail.com>
2022-01-12 11:41:19 +0200

    initial

---
+++ src/test/java/rayno/hattingh/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rayno.hattingh.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
